60c2471c834061b6a1ef00eb2801a07f90c62faf
eduardo camargo <eduardocamargo24358@gmail.com>
2026-04-15 13:20:32 -0300

    adicionando simuladorLogs

--- SimuladorLogs/SimuladorLogs.py
+++ SimuladorLogs/SimuladorLogs.py
@@ -10,3 +10,31 @@ Gerar alertas simples (ex.: salvar em arquivo ou enviar e-mail).
 
 """""
 
+
+
+falhas_login500 = 0
+falhas_login401 = 0
+falhas_login300 = 0
+acesso_200 = 0
+with open("access.log", "r") as f:
+    for linhas in f:
+        if "500" in linhas:
+            falhas_login500 += 1
+            print("O servidor recebeu uma resposta inválida de outro servidor.")
+        elif "401" in linhas:
+            falhas_login401 += 1
+            print("Falta autenticação ou credenciais inválidas.")
+        elif "300" in linhas:
+            falhas_login300 += 1
+            print("Redirecionamento temporário.")
+        elif "200" in linhas:
+            acesso_200 += 1
+            print("Requisição bem-sucedida, o conteúdo foi entregue normalmente.")
+
+
+print("\nResumo:")
+print(f"500: {falhas_login500}")
+print(f"401: {falhas_login401}")
+print(f"300: {falhas_login300}")
+print(f"200: {acesso_200}")
+
